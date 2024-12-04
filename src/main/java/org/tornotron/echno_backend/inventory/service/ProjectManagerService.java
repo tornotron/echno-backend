54d70337c648f6934c37d2ad1eeeebef18c6587e
hrishikesh <hrishikeshajith0@gmail.com>
2024-12-04 21:37:47 +0530

    temp commit

--- src/main/java/org/tornotron/echno_backend/inventory/service/ProjectManagerService.java
+++ src/main/java/org/tornotron/echno_backend/inventory/service/ProjectManagerService.java
@@ -1,26 +1,26 @@
-package org.tornotron.echno_backend.inventory.service;
-
-import org.springframework.stereotype.Service;
-import org.tornotron.echno_backend.inventory.Repository.ProjectManagerRepository;
-import org.tornotron.echno_backend.inventory.model.ProjectManager;
-
-
-@Service
-public class ProjectManagerService {
-
-    private final ProjectManagerRepository repository;
-
-    public ProjectManagerService(ProjectManagerRepository repository) {
-        this.repository = repository;
-    }
-
-    public boolean addProjectManager(ProjectManager projectManager) {
-        try {
-            repository.save(projectManager);
-
-        } catch (Exception e) {
-
-        }
-    }
-
-}
+//package org.tornotron.echno_backend.inventory.service;
+//
+//import org.springframework.stereotype.Service;
+//import org.tornotron.echno_backend.inventory.Repository.ProjectManagerRepository;
+//import org.tornotron.echno_backend.inventory.model.ProjectManager;
+//
+//
+//@Service
+//public class ProjectManagerService {
+//
+//    private final ProjectManagerRepository repository;
+//
+//    public ProjectManagerService(ProjectManagerRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    public boolean addProjectManager(ProjectManager projectManager) {
+//        try {
+//            repository.save(projectManager);
+//
+//        } catch (Exception e) {
+//
+//        }
+//    }
+//
+//}
