2107d7bf8b28ad86e07d9e9d44274eee5d251899
hrishikesh <hrishikeshajith0@gmail.com>
2024-12-20 20:52:54 +0530

    Added Repository interface for ProjectInviteCode

---
+++ src/main/java/org/tornotron/echno_backend/projectInviteCode/ProjectInviteCodeRepository.java
@@ -0,0 +1,6 @@
+package org.tornotron.echno_backend.projectInviteCode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectInviteCodeRepository extends JpaRepository<ProjectInviteCode,Long> {
+}
