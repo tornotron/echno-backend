e056bd90b9827a7f8fa67585d1653ee805929352
hrishikesh <hrishikeshajith0@gmail.com>
2025-01-08 20:44:43 +0530

    Added explicit column annotations

--- src/main/java/org/tornotron/echno_backend/project/Project.java
+++ src/main/java/org/tornotron/echno_backend/project/Project.java
@@ -15,27 +15,33 @@ import java.util.List;
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "Project")
 public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
     private Long id;
 
     @NotBlank(message = "projectName is required")
     @Size(min = 3,max = 50,message = "projectName must be between 3 and 50 characters")
-    @Column(unique = true)
+    @Column(name = "project_name", unique = true,nullable = true)
     private String projectName;
 
     @NotBlank(message = "projectAddress is required")
     @Size(min = 3,max = 50,message = "projectAddress must be between 3 and 50 characters")
+    @Column(name = "project_address", nullable = true)
     private String projectAddress;
 
     @CreationTimestamp
+    @Column(name = "created_at", nullable = true)
     private LocalDateTime createdAt;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = true)
     private ProjectCreationStatus status;
 
     @OneToMany(mappedBy = "project")
+    @Column(nullable = true)
     private List<TeamMember> teamMembers;
 }
--- src/main/java/org/tornotron/echno_backend/teamMember/TeamMember.java
+++ src/main/java/org/tornotron/echno_backend/teamMember/TeamMember.java
@@ -1,6 +1,7 @@
 package org.tornotron.echno_backend.teamMember;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -10,17 +11,19 @@ import org.tornotron.echno_backend.project.Project;
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "Team_member")
 public class TeamMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
     private Long id;
 
-    @NotNull(message = "memberName is required")
+    @NotBlank(message = "memberName is required")
     @Size(min = 3,max = 50,message = "memberName must be between 3 and 50 characters")
-    @Column(unique = true)
+    @Column(name = "member_name",unique = true)
     private String memberName;
 
-    @Column(unique = true)
+    @Column(unique = true,name = "member_email")
     private String memberEmail;
 
     @ManyToOne
