e5042e3e70d9f5a292079f57f768517a263a8e3d
hrishikesh <hrishikeshajith0@gmail.com>
2025-02-06 01:24:25 +0530

    Added InvalidAttendanceSequenceException, InvalidPhotoException, InvalidRequestException

---
+++ src/main/java/org/tornotron/echno_backend/common/exception/InvalidPhotoException.java
@@ -0,0 +1,7 @@
+package org.tornotron.echno_backend.common.exception;
+
+public class InvalidPhotoException extends RuntimeException {
+    public InvalidPhotoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/tornotron/echno_backend/common/exception/InvalidRequestException.java
@@ -0,0 +1,7 @@
+package org.tornotron.echno_backend.common.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
