ee2d053cf1e5c1be1b7f665f01774c8b1116873f
hrishikesh <hrishikeshajith0@gmail.com>
2025-02-06 01:23:23 +0530

    Improved Error response for validation

--- src/main/java/org/tornotron/echno_backend/common/response/ErrorResponseValidation.java
+++ src/main/java/org/tornotron/echno_backend/common/response/ErrorResponseValidation.java
@@ -2,15 +2,22 @@ package org.tornotron.echno_backend.common.response;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Data
 public class ErrorResponseValidation {
+    private int status;
     private String message;
     private Map<String ,String> errors;
+    private String details;
+    private LocalDateTime timestamp;
 
-    public ErrorResponseValidation(String message,Map<String ,String> errors) {
+    public ErrorResponseValidation(int status, String message, Map<String, String> errors, String details, LocalDateTime timestamp) {
+        this.status = status;
         this.message = message;
         this.errors = errors;
+        this.details = details;
+        this.timestamp = timestamp;
     }
 }
