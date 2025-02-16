4488d26b88b5536cd2fbd73587f4344588703305
hrishikesh <hrishikeshajith0@gmail.com>
2025-02-16 21:40:30 +0530

    Renamed AttendanceRecordDto.java to AttendanceCreationDto.java

--- src/main/java/org/tornotron/echno_backend/attendance/dto/AttendanceCreationDto.java
+++ src/main/java/org/tornotron/echno_backend/attendance/dto/AttendanceCreationDto.java
@@ -3,14 +3,12 @@ package org.tornotron.echno_backend.attendance.dto;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.tornotron.echno_backend.attendance.enums.RecordType;
-import org.tornotron.echno_backend.common.embed.GeoLocation;
 
 @Data
-public class AttendanceRecordDto {
+public class AttendanceCreationDto {
 
     @NotBlank
     @Size(min = 3,max = 50,message = "employeeName must be between 3 and 50 characters")
@@ -20,13 +18,11 @@ public class AttendanceRecordDto {
     @Size(min = 3,max = 50,message = "location must be between 3 and 50 characters")
     private String location;
 
-    @NotNull
-    @Size(min = 3,max = 20,message = "recordType must be between 3 and 20 characters")
     @Enumerated(EnumType.STRING)
     private RecordType recordType;
 
-    @NotNull(message = "must not be blank")
-    private GeoLocation geoLocation;
+    @NotBlank
+    private String geoLocation;
 
     @NotBlank
     private String deviceInfo;
