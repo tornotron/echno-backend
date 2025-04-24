c96fcb48871dde0308043654087e3c1bf708230d
hrishikesh <hrishikeshajith0@gmail.com>
2025-04-25 02:06:24 +0530

    converted api request parameters to request body

--- src/main/java/org/tornotron/echno_backend/projectInviteCode/dto/InviteCodeValidationDto.java
+++ src/main/java/org/tornotron/echno_backend/projectInviteCode/dto/InviteCodeValidationDto.java
@@ -1,10 +1,13 @@
 package org.tornotron.echno_backend.projectInviteCode.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class InviteCodeValidationDto {
+
     @NotNull(message = "code is a required request parameter")
-    int code;
+    @Size(min = 5,max = 5,message = "code must be exactly 5 characters")
+    String code;
 }
