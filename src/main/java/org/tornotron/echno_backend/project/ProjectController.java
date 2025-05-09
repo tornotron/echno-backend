9f09295f8dde7acc4f6d4f6aa724abd9bc959f5d
hrishikesh <hrishikeshajith0@gmail.com>
2025-05-09 17:50:01 +0530

    renamed endpoint from projects to project

--- src/main/java/org/tornotron/echno_backend/project/ProjectController.java
+++ src/main/java/org/tornotron/echno_backend/project/ProjectController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/projects")
+@RequestMapping("/api/v1/project")
 @Validated
 public class ProjectController {
 
