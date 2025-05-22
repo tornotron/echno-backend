a81c2a209164ad2086da9d37f789d486924df59b
hrishikesh <hrishikeshajith0@gmail.com>
2025-05-22 22:04:27 +0530

    Deleted an unwanted dto

--- src/main/java/org/tornotron/echno_backend/organization/dto/ProjectDtoForOrg.java
+++
@@ -1,9 +0,0 @@
-package org.tornotron.echno_backend.organization.dto;
-
-import lombok.Data;
-
-@Data
-public class ProjectDtoForOrg {
-    private String projectName;
-    private String projectAddress;
-}
