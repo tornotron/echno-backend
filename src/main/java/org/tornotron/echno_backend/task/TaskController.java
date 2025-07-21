9a76d0a9e88588bbb3166656a0e88cbeaa653479
hrishikesh <hrishikeshajith0@gmail.com>
2025-07-21 19:15:55 +0530

    Refactored Task entity

--- src/main/java/org/tornotron/echno_backend/task/TaskController.java
+++ src/main/java/org/tornotron/echno_backend/task/TaskController.java
@@ -1,6 +1,9 @@
 package org.tornotron.echno_backend.task;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +11,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.tornotron.echno_backend.common.response.ApiResponse;
+import org.tornotron.echno_backend.task.dto.TaskCreationDto;
 import org.tornotron.echno_backend.task.dto.TaskDto;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -18,43 +23,54 @@ import java.util.List;
 public class TaskController {
 
     private final TaskService service;
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     public TaskController(TaskService service) {
         this.service = service;
     }
 
-    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE} )
-    public ResponseEntity<ApiResponse> createTask(
-                                             @RequestPart("photo") MultipartFile photo,
-                                             @RequestPart("task") String taskDtoString
-    ) {
-        service.addTask(taskDtoString,photo);
+    @PostMapping
+    public ResponseEntity<ApiResponse> createTask(@Valid @RequestBody TaskCreationDto taskCreationDto) {
+        service.addTask(taskCreationDto);
+        logger.info("Task Added Successfully");
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Task Created Successfully"));
     }
 
     @GetMapping
-    public ResponseEntity<List<TaskDto>> readAllTasks() {
-        return new ResponseEntity<>(service.getAllTasks(), HttpStatus.OK);
+    public ResponseEntity<List<TaskDto>> readAllTasks(@RequestParam(defaultValue = "0") int pageNo,
+                                                      @RequestParam(defaultValue = "10") int pageSize) {
+        Page<TaskDto> tasks = service.getAllTasks(pageNo, pageSize);
+        logger.info("All Tasks Retrieved Successfully");
+        return new ResponseEntity<>(tasks.getContent(), HttpStatus.OK);
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<?> readATask(@PathVariable Long id) {
-        TaskDto task = service.getATask(id);
-        return new ResponseEntity<>(task, HttpStatus.OK);
-    }
+//    @GetMapping("{id}")
+//    public ResponseEntity<?> readATask(@PathVariable Long id) {
+//        TaskDto task = service.getATask(id);
+//        logger.info("Task with id: {} Retrieved Successfully", id);
+//        return new ResponseEntity<>(task, HttpStatus.OK);
+//    }
+//
+//    @PatchMapping("{id}")
+//    public ResponseEntity<ApiResponse> partialUpdateATask(@RequestBody Map<String, Object> updates, @PathVariable Long id) {
+//        service.partialUpdateATask(updates, id);
+//        logger.info("Task with id: {} Updated Successfully", id);
+//        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Task with id: " + id + " updated"));
+//    }
+//
+//
+//    @PatchMapping("/batch")
+//    public ResponseEntity<ApiResponse> batchUpdateTasks(@Valid @RequestBody List<TaskPatchDto> updates) {
+//        service.batchUpdateTasks(updates);
+//        logger.info("Batch Update of Tasks Completed Successfully");
+//        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Batch Update of Tasks Completed Successfully"));
+//    }
+//
+//    @DeleteMapping("{id}")
+//    public ResponseEntity<ApiResponse> deleteTask(@PathVariable Long id) {
+//       service.deleteATask(id);
+//        logger.info("Task with id: {} Deleted Successfully", id);
+//        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Task with id: " + id + " has been deleted"));
+//    }
 
-    @PutMapping("{id}")
-    public ResponseEntity<ApiResponse> updateTask(@Valid @RequestBody Task updatedTask, @PathVariable Long id) {
-        boolean updated = service.updateATask(updatedTask, id);
-        if (updated) {
-            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Task with id: " + id + " has been updated"));
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Task with id: " + id + " not found"));
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<ApiResponse> deleteTask(@PathVariable Long id) {
-        service.deleteATask(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Task with id: " + id + " has been deleted"));
-    }
 }
--- src/main/java/org/tornotron/echno_backend/task/dto/TaskDto.java
+++ src/main/java/org/tornotron/echno_backend/task/dto/TaskDto.java
@@ -1,11 +1,29 @@
 package org.tornotron.echno_backend.task.dto;
 
 import lombok.Data;
+import org.tornotron.echno_backend.category.Category;
+import org.tornotron.echno_backend.category.dto.CategoryDto;
+import org.tornotron.echno_backend.employee.Employee;
+import org.tornotron.echno_backend.employee.dto.EmployeeDto;
+import org.tornotron.echno_backend.project.dto.ProjectDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Data
 public class TaskDto {
-    private String taskName;
-    private String categories;
-    private byte[] photo;
-    private Integer progress;
+    private Long id;
+    private String title;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Long employeeId;
+    private Long projectId;
+    private Set<Long> assigneeIds;
+    private Long categoryId;
+    private Double progress;
+    private List<String> tags;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String status;
 }
