1455ff98685db2ede8e4ef4409ba05ee419b226d
hrishikesh <hrishikeshajith0@gmail.com>
2025-07-21 19:15:55 +0530

    Refactored Task entity

--- src/main/java/org/tornotron/echno_backend/task/TaskService.java
+++ src/main/java/org/tornotron/echno_backend/task/TaskService.java
@@ -5,13 +5,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.ValidationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.tornotron.echno_backend.category.Category;
+import org.tornotron.echno_backend.category.CategoryRepository;
+import org.tornotron.echno_backend.category.CategoryService;
 import org.tornotron.echno_backend.common.exception.DatabaseOperationException;
-import org.tornotron.echno_backend.common.exception.InvalidPhotoException;
 import org.tornotron.echno_backend.common.exception.InvalidRequestException;
 import org.tornotron.echno_backend.common.exception.ResourceNotFoundException;
+import org.tornotron.echno_backend.employee.Employee;
+import org.tornotron.echno_backend.employee.EmployeeRepository;
+import org.tornotron.echno_backend.employee.EmployeeService;
+import org.tornotron.echno_backend.employee.dto.EmployeeDto;
+import org.tornotron.echno_backend.project.ProjectRepository;
+import org.tornotron.echno_backend.project.ProjectService;
 import org.tornotron.echno_backend.task.dto.TaskCreationDto;
 import org.tornotron.echno_backend.task.dto.TaskDto;
 
@@ -21,118 +32,105 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 public class TaskService {
+
     private final TaskRepository taskRepository;
-    private static final long MAX_FILE_SIZE=10*1024*1024;
-    private static final String[] ALLOWED_IMAGE_TYPES = {"image/jpeg", "image/png", "image/jpg"};
-    private final ObjectMapper objectMapper;
+    private final EmployeeRepository employeeRepository;
+    private final ProjectRepository projectRepository;
+    private final CategoryRepository categoryRepository;
 
-    public TaskService(TaskRepository taskRepository, ObjectMapper objectMapper) {
+
+    public TaskService(TaskRepository taskRepository,
+                       EmployeeRepository employeeRepository,
+                       ProjectRepository projectRepository,
+                       CategoryRepository categoryRepository
+                      ) {
         this.taskRepository = taskRepository;
-        this.objectMapper = objectMapper;
+        this.employeeRepository = employeeRepository;
+        this.projectRepository = projectRepository;
+        this.categoryRepository = categoryRepository;
     }
 
-    private void validatePhoto(MultipartFile photo) {
-        if(photo.isEmpty()) {
-            throw new InvalidPhotoException("Photo file is empty");
-        }
-        if(photo.getSize() > MAX_FILE_SIZE) {
-            throw new InvalidPhotoException("File size exceeds maximum limit of 10MB");
-        }
-        String contentType = photo.getContentType();
-        if(contentType == null || !Arrays.asList(ALLOWED_IMAGE_TYPES).contains(contentType)) {
-            throw new InvalidPhotoException("Invalid file type. Allowed types: JPEG, PNG");
-        }
-    }
-
-    private TaskCreationDto parseAndValidateTaskDto(String taskDtoString) {
-        try {
-
-
-            TaskCreationDto taskDto = objectMapper.readValue(taskDtoString, TaskCreationDto.class);
-
-            Set<ConstraintViolation<TaskCreationDto>> violations = Validation.buildDefaultValidatorFactory()
-                    .getValidator()
-                    .validate(taskDto);
-            if(!violations.isEmpty()) {
-                Map<String,String> validationErrors = violations.stream()
-                        .collect(Collectors.toMap(
-                                violation -> violation.getPropertyPath().toString(),
-                                ConstraintViolation::getMessage
-                        ));
-                throw new ValidationException("Invalid task data");
-            }
-            return taskDto;
-        } catch (JsonProcessingException e) {
-            throw new InvalidRequestException("Invalid Json format in task");
-        }
-    }
 
     private TaskDto convertToDto(Task task) {
-        TaskDto taskDto = new TaskDto();
-        taskDto.setTaskName(task.getTaskName());
-        taskDto.setCategories(task.getCategories());
-        taskDto.setProgress(task.getProgress());
-        return taskDto;
+        TaskDto dto = new TaskDto();
+        dto.setId(task.getId());
+        dto.setTitle(task.getTitle());
+        dto.setStartDate(task.getStartDate());
+        dto.setEndDate(task.getEndDate());
+        dto.setEmployeeId(task.getEmployee().getId());
+        dto.setProjectId(task.getProject().getId());
+        dto.setAssigneeIds(task.getAssignees().stream()
+                .map(Employee::getId)
+                .collect(Collectors.toSet()));
+        dto.setCategoryId(task.getCategory().getId());
+        dto.setProgress(task.getProgress());
+        dto.setTags(task.getTags());
+        dto.setCreatedAt(task.getCreatedAt());
+        dto.setUpdatedAt(task.getUpdatedAt());
+        dto.setStatus(task.getStatus());
+        return dto;
     }
 
-    public void addTask(String taskDtoString, MultipartFile photo) {
-        TaskCreationDto taskCreationDto = parseAndValidateTaskDto(taskDtoString);
+    public void addTask(TaskCreationDto taskCreationDto) {
         Task task = new Task();
-        task.setTaskName(taskCreationDto.getTaskName());
-        task.setCategories(taskCreationDto.getCategories());
+        task.setTitle(taskCreationDto.getTitle());
+        task.setStartDate(taskCreationDto.getStartDate());
+        task.setEndDate(taskCreationDto.getEndDate());
         task.setProgress(taskCreationDto.getProgress());
-        try {
-        if(photo != null) {
-            validatePhoto(photo);
-            task.setPhoto(photo.getBytes());
+        task.setStatus(taskCreationDto.getStatus());
+
+        if (taskCreationDto.getEmployeeId() != null) {
+            task.setEmployee(employeeRepository.findById(taskCreationDto.getEmployeeId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + taskCreationDto.getEmployeeId())));
+        } else {
+            throw new InvalidRequestException("Employee ID must be provided");
         }
+        if (taskCreationDto.getProjectId() != null) {
+            task.setProject(projectRepository.findById(taskCreationDto.getProjectId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Project not found with id: " + taskCreationDto.getProjectId())));
+        } else {
+            throw new InvalidRequestException("Project ID must be provided");
+        }
+
+        if(taskCreationDto.getAssigneeIds() != null && !taskCreationDto.getAssigneeIds().isEmpty()) {
+            Set<Employee> assignees = new HashSet<>();
+            for(Long assigneeId : taskCreationDto.getAssigneeIds()) {
+                Employee assignee = employeeRepository.findById(assigneeId).
+                        orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + assigneeId));
+                assignees.add(assignee);
+            }
+            task.setAssignees(assignees);
+
+        }
+
+        if(taskCreationDto.getCategory() != null) {
+            task.setCategory(categoryRepository.findCategoryByName(taskCreationDto.getCategory())
+                    .orElseThrow(() -> new ResourceNotFoundException("Category not found with name: " + taskCreationDto.getCategory())));
+        } else {
+            throw new InvalidRequestException("Category must be provided");
+        }
+
+        if(taskCreationDto.getTags() != null && !taskCreationDto.getTags().isEmpty()) {
+            List<String> cleanedTags = taskCreationDto.getTags().stream()
+                    .filter(tag -> tag != null && !tag.trim().isEmpty())
+                    .map(String::trim)
+                    .distinct()
+                    .toList();
+            task.setTags(cleanedTags);
+        }
+
+        try {
             taskRepository.save(task);
         } catch (Exception e) {
-            throw new DatabaseOperationException("Task could not be saved");
+            throw new DatabaseOperationException("Error while adding task: " + e.getMessage());
         }
     }
 
     @Transactional(readOnly = true)
-    public List<TaskDto> getAllTasks() {
-        return taskRepository.findAll().stream()
-                .map(this::convertToDto)
-                .collect(Collectors.toList());
+    public Page<TaskDto> getAllTasks(int pageNo, int pageSize) {
+        Pageable pageable = Pageable.ofSize(pageSize).withPage(pageNo);
+        return taskRepository.findAll(pageable)
+                .map(this::convertToDto);
     }
-
-    @Transactional(readOnly = true)
-    public TaskDto getATask(Long id) {
-        TaskDto taskDto = taskRepository.findById(id)
-                .map(this::convertToDto)
-                .orElse(null);
-        if(taskDto == null) {
-            throw new ResourceNotFoundException("Task not found with id: "+id);
-        } else {
-            return taskDto;
-        }
-    }
-
-    public Boolean updateATask(Task updatedTask,Long id) {
-        if(updatedTask == null) {
-            return null;
-        }
-        Optional<Task> optionalTask = taskRepository.findById(id);
-        if(optionalTask.isPresent()) {
-            Task taskObj = optionalTask.get();
-            taskObj.setTaskName(updatedTask.getTaskName());
-            taskObj.setCategories(updatedTask.getCategories());
-            taskObj.setProgress(updatedTask.getProgress());
-            taskRepository.save(taskObj);
-            return true;
-        }
-        return false;
-    }
-
-    public void deleteATask(Long id) {
-        if(!taskRepository.existsById(id)) {
-            throw new ResourceNotFoundException("Task not found with id: "+id);
-        }
-        taskRepository.deleteById(id);
-    }
-
 
 }
