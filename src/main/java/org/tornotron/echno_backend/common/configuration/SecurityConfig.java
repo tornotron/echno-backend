443054845474f92d8c8199120a6323484c873ff1
hrishikesh <hrishikeshajith0@gmail.com>
2025-07-22 14:04:37 +0530

    Turned authentication off

--- src/main/java/org/tornotron/echno_backend/common/configuration/SecurityConfig.java
+++ src/main/java/org/tornotron/echno_backend/common/configuration/SecurityConfig.java
@@ -25,8 +25,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                         .authorizeHttpRequests(auth -> auth
                                 .anyRequest()
-                                .authenticated()
-//                                        .permitAll()
+//                                .authenticated()
+                                        .permitAll()
                         )
                         .oauth2ResourceServer(oauth2 -> oauth2
                                 .jwt(Customizer.withDefaults())
