c0defdd4ffaa46e352aabbfebcbf04c92d81d545
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-08 15:58:36 +0530

    added more fields to Organization entity

--- src/main/java/org/tornotron/echno_backend/organization/Organization.java
+++ src/main/java/org/tornotron/echno_backend/organization/Organization.java
@@ -4,6 +4,7 @@ package org.tornotron.echno_backend.organization;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.tornotron.echno_backend.employee.Employee;
 import org.tornotron.echno_backend.project.Project;
 
 import java.time.LocalDateTime;
@@ -22,11 +23,23 @@ public class Organization {
     @Column(name = "id",nullable = false)
     private Long id;
 
-    @Column(name = "organization_name", unique = true,nullable = true)
+    @Column(name = "organization_name", unique = true,nullable = false)
     private String organizationName;
 
-    @Column(name = "organization_address", nullable = true)
+    @Column(name = "organization_address", nullable = false)
     private String organizationAddress;
+
+    @Column(name = "organization_email",nullable = false)
+    private String organizationEmail;
+
+    @Column(name = "organization_phone", nullable = false)
+    private String organizationPhone;
+
+    @Column(name = "organization_website", nullable = true)
+    private String organizationWebsite;
+
+    @Column(name = "organization_logo", nullable = true)
+    private String organizationLogo;
 
     @Column(name = "created_at", nullable = true)
     private LocalDateTime createdAt;
@@ -34,4 +47,14 @@ public class Organization {
     @OneToMany(mappedBy = "organization")
     @Column(nullable = true)
     private List<Project> projects;
+
+    @OneToMany(mappedBy = "organization")
+    @Column(nullable = true)
+    private List<Employee> employees;
+
+    @Column(name = "creator_id", nullable = true)
+    private Integer creatorId;
+
+    @Column(name = "is_active", nullable = true)
+    private Boolean isActive;
 }
