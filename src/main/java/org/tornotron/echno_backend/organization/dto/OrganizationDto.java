fa99b0c1d2b1b03438a9d9524f01cb4075624677
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-08 16:01:12 +0530

    added more fields to dto

--- src/main/java/org/tornotron/echno_backend/organization/dto/OrganizationDto.java
+++ src/main/java/org/tornotron/echno_backend/organization/dto/OrganizationDto.java
@@ -1,6 +1,7 @@
 package org.tornotron.echno_backend.organization.dto;
 
 import lombok.Data;
+import org.tornotron.echno_backend.employee.dto.EmployeeDto;
 import org.tornotron.echno_backend.project.dto.ProjectDto;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,11 @@ public class OrganizationDto {
     private Long id;
     private String organizationName;
     private String organizationAddress;
+    private String organizationEmail;
+    private String organizationPhone;
+    private String organizationWebsite;
+    private String organizationLogo ;
     private LocalDateTime createdAt;
     private List<ProjectDto> projects;
+    private List<EmployeeDto> employees;
 }
