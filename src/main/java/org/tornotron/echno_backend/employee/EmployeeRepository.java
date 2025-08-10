7e3aa73494ea0fea43a9ac2f5f1489c2b94bcfba
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-10 22:40:49 +0530

    Added new Employee fields and corresponding changes

--- src/main/java/org/tornotron/echno_backend/employee/EmployeeRepository.java
+++ src/main/java/org/tornotron/echno_backend/employee/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package org.tornotron.echno_backend.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.tornotron.echno_backend.organization.Organization;
+import org.tornotron.echno_backend.user.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     Optional<Employee> findEmployeeByEmployeeName(String employeeName);
+    boolean existsByUserAndOrganization(User user, Organization organization);
 }
