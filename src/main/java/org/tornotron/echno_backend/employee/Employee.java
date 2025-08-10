bf0d406a421afb16dc9d5e78d68dd606285f31a8
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-10 22:40:49 +0530

    Added new Employee fields and corresponding changes

--- src/main/java/org/tornotron/echno_backend/employee/Employee.java
+++ src/main/java/org/tornotron/echno_backend/employee/Employee.java
@@ -3,8 +3,10 @@ package org.tornotron.echno_backend.employee;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.tornotron.echno_backend.employee.enums.EmployeeStatus;
 import org.tornotron.echno_backend.organization.Organization;
 import org.tornotron.echno_backend.task.Task;
+import org.tornotron.echno_backend.user.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,7 +19,34 @@ public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "designation", nullable = false)
+    private String designation;
+
+    @Column(name = "department", nullable = false)
+    private String department;
+
+    @Column(name = "joining_date", nullable = true)
+    private LocalDateTime joiningDate;
+
+    @Column(name = "salary", nullable = true)
+    private Double salary;
+
+    @Column(name = "reporting_manager", nullable = true)
+    private String reportingManager;
+
+    @Column(name = "shift_timing", nullable = true)
+    private String shiftTiming;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private EmployeeStatus status;
+
+//    @Column(name = "certifications", nullable = true)
+//    private List<String> certifications;
+
 
     @Column(name = "employee_name",nullable = false)
     private String employeeName;
@@ -40,6 +69,11 @@ public class Employee {
     @ManyToMany(mappedBy = "assignees")
     private List<Task> assignedTasks;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id", nullable = false)
     private Organization organization;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
---
+++ src/main/java/org/tornotron/echno_backend/employee/enums/EmployeeStatus.java
@@ -0,0 +1,11 @@
+package org.tornotron.echno_backend.employee.enums;
+
+public enum EmployeeStatus {
+    active,
+    inactive,
+    onLeave,
+    terminated,
+    resigned,
+    probation,
+    suspended,
+}
