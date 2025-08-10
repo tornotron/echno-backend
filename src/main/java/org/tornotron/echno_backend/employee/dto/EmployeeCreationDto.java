75d3dd242abb49a83ada6dace6c256a32b8c5b36
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-10 22:40:49 +0530

    Added new Employee fields and corresponding changes

--- src/main/java/org/tornotron/echno_backend/employee/dto/EmployeeCreationDto.java
+++ src/main/java/org/tornotron/echno_backend/employee/dto/EmployeeCreationDto.java
@@ -1,5 +1,7 @@
 package org.tornotron.echno_backend.employee.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
@@ -8,19 +10,40 @@ import java.time.LocalDateTime;
 @Data
 public class EmployeeCreationDto {
 
-    @NotBlank
+    @NotBlank(message = "designation is required")
+    @Size(min = 3, max = 50, message = "designation must be between 3 and 50 characters")
+    private String designation;
+
+    @NotBlank(message = "department is required")
+    @Size(min = 3, max = 50, message = "department must be between 3 and 50 characters")
+    private String department;
+
+    private LocalDateTime joiningDate;
+
+    private Double salary;
+
+    private String reportingManager;
+
+    private String shiftTiming;
+
+    @NotBlank(message = "status is required")
+    @Size(min = 3, max = 50, message = "status must be between 3 and 50 characters")
+    @Enumerated(EnumType.STRING)
+    private String status;
+
+    @NotBlank(message = "employeeName is required")
     @Size(min = 3,max = 50,message = "employeeName must be between 3 and 50 characters")
     private String employeeName;
 
-    @NotBlank
+    @NotBlank(message = "message is required")
     @Size(min = 3,max = 10,message = "gender must be between 3 and 10 characters")
     private String gender;
 
-    @NotBlank
+    @NotBlank(message = "phoneNumber is required")
     @Size(min = 10,max = 10,message = "phoneNumber must be of 10 characters")
     private String phoneNumber;
 
-    @NotBlank
+    @NotBlank(message = "emailAddress is required")
     @Size(max = 255)
     @Pattern(
             regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$",
@@ -28,7 +51,7 @@ public class EmployeeCreationDto {
     )
     private String emailAddress;
 
-    @NotNull(message = "must not be blank")
+    @NotNull(message = "dateOfBirth is required")
     private LocalDateTime dateOfBirth;
 
     @Size(max = 255, message = "organizationName must be at most 255 characters")
