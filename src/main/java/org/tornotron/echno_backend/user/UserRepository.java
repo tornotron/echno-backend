130a24e39d01a4c8469fa523d8f9065cd3e2ab6e
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-12 01:03:24 +0530

    Added fetching a user and deleting a user feature

--- src/main/java/org/tornotron/echno_backend/user/UserRepository.java
+++ src/main/java/org/tornotron/echno_backend/user/UserRepository.java
@@ -2,7 +2,6 @@ package org.tornotron.echno_backend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 }
