2749162703861f1f9d28aa706dae9e7ca29117b7
hrishikesh <hrishikeshajith0@gmail.com>
2025-08-16 14:41:36 +0530

    Added feature to fetch all organization which the user is part of

--- src/main/java/org/tornotron/echno_backend/user/UserRepository.java
+++ src/main/java/org/tornotron/echno_backend/user/UserRepository.java
@@ -1,7 +1,14 @@
 package org.tornotron.echno_backend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.tornotron.echno_backend.organization.Organization;
+
+import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT DISTINCT e.organization FROM Employee  e WHERE e.user.id = :userId")
+    List<Organization> findOrganizationsByUserId(@Param("userId") Long userId);
 }
