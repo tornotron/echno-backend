8c8a4e3b8cb5b3f75ef3c5c8fe68675f6559987f
hrishikesh <hrishikeshajith0@gmail.com>
2025-09-15 01:20:50 +0530

    Added Employee details field

--- src/main/java/org/tornotron/echno_backend/projectInviteCode/dto/InviteCodeGenerationDto.java
+++ src/main/java/org/tornotron/echno_backend/projectInviteCode/dto/InviteCodeGenerationDto.java
@@ -1,17 +1,44 @@
 package org.tornotron.echno_backend.projectInviteCode.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class InviteCodeGenerationDto {
 
-    @NotNull(message = "projectName is a required request parameter")
+    @NotNull(message = "projectName is required")
     @Size(min = 3,max = 50,message = "projectName must be between 3 and 50 characters")
     String projectName;
 
     int maxUses = 1;
 
     int validityDays = 5;
+
+    @NotBlank(message = "designation is required")
+    @Size(min = 3, max = 50, message = "designation must be between 3 and 50 characters")
+    private String designation;
+
+    @NotBlank(message = "department is required")
+    @Size(min = 3, max = 50, message = "department must be between 3 and 50 characters")
+    private String department;
+
+    private LocalDateTime joiningDate;
+
+    private Double salary;
+
+    private String reportingManager;
+
+    private String shiftTiming;
+
+    @NotBlank(message = "status is required")
+    @Size(min = 3, max = 50, message = "status must be between 3 and 50 characters")
+    @Enumerated(EnumType.STRING)
+    private String status;
+
 }
