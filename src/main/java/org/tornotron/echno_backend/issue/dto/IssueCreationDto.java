01d15a7318cc7e75e661c1a01c65f4598892b4a3
hrishikesh <hrishikeshajith0@gmail.com>
2025-09-28 16:44:08 +0530

    Implement issue pagination and partial update functionality; add task association in issue creation

--- src/main/java/org/tornotron/echno_backend/issue/dto/IssueCreationDto.java
+++ src/main/java/org/tornotron/echno_backend/issue/dto/IssueCreationDto.java
@@ -14,6 +14,8 @@ public class IssueCreationDto {
     @Size(min = 3,max = 50,message = "Title must be between 3 and 50 characters")
     private String title;
 
+    private Long taskId;
+
     @NotBlank
     @Size(min = 5, max = 500, message = "Description must be between 10 and 500 characters")
     private String description;
