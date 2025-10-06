033529821881a9f467272ec5bd7a3d05c64bcbac
hrishikesh <hrishikeshajith0@gmail.com>
2025-10-06 23:41:55 +0530

    Enable caching in the application and adjust logging levels for Bucket4j

--- src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
+++ src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
@@ -2,8 +2,10 @@ package org.tornotron.echno_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EchnoBackendApplication {
 
     public static void main(String[] args) {
