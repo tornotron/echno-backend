1a9d7cf69fa8b1f6ba4a57b837d127a72eafe632
hrishikesh <hrishikeshajith0@gmail.com>
2025-10-08 21:57:29 +0530

    Caching annotation added

--- src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
+++ src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
@@ -2,6 +2,7 @@ package org.tornotron.echno_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
