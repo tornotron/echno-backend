2f014d134d6e975fde66b4a8683c64d48abe5dd8
hrishikesh <hrishikeshajith0@gmail.com>
2025-10-09 15:43:30 +0530

    Enable authentication for all requests in SecurityConfig

--- src/main/java/org/tornotron/echno_backend/common/configuration/SecurityConfig.java
+++ src/main/java/org/tornotron/echno_backend/common/configuration/SecurityConfig.java
@@ -25,8 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                         .authorizeHttpRequests(auth -> auth
                                 .anyRequest()
-//                                .authenticated()
-                                        .permitAll()
+                                .authenticated()
                         )
                         .oauth2ResourceServer(oauth2 -> oauth2
                                 .jwt(Customizer.withDefaults())
