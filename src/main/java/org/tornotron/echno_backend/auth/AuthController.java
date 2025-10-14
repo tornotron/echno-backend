f9492e141f1e28991f320dcac25d771d6257616b
hrishikesh <hrishikeshajith0@gmail.com>
2025-10-15 01:42:46 +0530

    Add AuthController for user registration endpoint

---
+++ src/main/java/org/tornotron/echno_backend/auth/AuthController.java
@@ -0,0 +1,29 @@
+package org.tornotron.echno_backend.auth;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.tornotron.echno_backend.user.UserService;
+import org.tornotron.echno_backend.user.dto.UserDto;
+import org.tornotron.echno_backend.user.dto.UserRegistrationDto;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> registerUser(@Valid @RequestBody UserRegistrationDto userRegistrationDto) {
+        UserDto createdUser = userService.registerUser(userRegistrationDto);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
