b3c38751f72e4b263ade0771ecc96aa772d77ac2
hrishikesh <hrishikeshajith0@gmail.com>
2025-10-16 11:41:41 +0530

    Keycloak config file generator added

---
+++ src/main/java/org/tornotron/echno_backend/common/configuration/KeycloakConfigGenerator.java
@@ -0,0 +1,117 @@
+package org.tornotron.echno_backend.common.configuration;
+
+import jakarta.annotation.PostConstruct;
+import org.aspectj.apache.bcel.util.ClassPath;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+@Component
+public class KeycloakConfigGenerator {
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${keycloak.web-origin}")
+    private String webOrigin;
+
+    @Value("${keycloak.admin.userName}")
+    private String adminUsername;
+
+    @Value("${keycloak.admin.email}")
+    private String adminEmail;
+
+    @Value("${keycloak.admin.password}")
+    private String adminPassword;
+
+    @Value("${keycloak.admin.firstName}")
+    private String adminFirstName;
+
+    @Value("${keycloak.admin.lastName}")
+    private String adminLastName;
+
+    @Value("${keycloak.service.userName}")
+    private String serviceUsername;
+
+    @Value("${keycloak.service.email}")
+    private String serviceEmail;
+
+    @Value("${keycloak.service.password}")
+    private String servicePassword;
+
+    @Value("${keycloak.service.firstName}")
+    private String serviceFirstName;
+
+    @Value("${keycloak.service.lastName}")
+    private String serviceLastName;
+
+    @Value("${keycloak.config.output-path}")
+    private String outputPath;
+
+    @PostConstruct
+    public void generateConfigs() {
+        try {
+            generateRealmConfig();
+            generateUsersConfig();
+            System.out.println("Keycloak configuration files generated successfully");
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to generate keycloak config files",e);
+        }
+    }
+
+    private void generateRealmConfig() throws IOException {
+        String template = readTemplate("init-keycloak.json.template");
+
+        String config = template
+                .replace("${KEYCLOAK_CLIENT_ID}",clientId)
+                .replace("${KEYCLOAK_REDIRECT_URI}",redirectUri)
+                .replace("${KEYCLOAK_WEB_ORIGIN}",webOrigin);
+
+
+        writeConfig("init-keycloak.json",config);
+
+    }
+
+    private void generateUsersConfig() throws IOException {
+        String template = readTemplate("init-keycloak-users.json.template");
+
+        String config = template
+                .replace("${ADMIN_USERNAME}",adminUsername)
+                .replace("${ADMIN_EMAIL}",adminEmail)
+                .replace("${ADMIN_PASSWORD}",adminPassword)
+                .replace("${ADMIN_FIRST_NAME}",adminFirstName)
+                .replace("${ADMIN_LAST_NAME}",adminLastName)
+                .replace("${SERVICE_USERNAME}",serviceUsername)
+                .replace("${SERVICE_EMAIL}",serviceEmail)
+                .replace("${SERVICE_PASSWORD}",servicePassword)
+                .replace("${SERVICE_FIRST_NAME}",serviceFirstName)
+                .replace("${SERVICE_LAST_NAME}",serviceLastName);
+
+        writeConfig("init-keycloak-users.json",config);
+    }
+
+    private String readTemplate(String filename) throws IOException {
+        ClassPathResource resource = new ClassPathResource("keycloak-templates/"+filename);
+        return new String(resource.getInputStream().readAllBytes());
+    }
+
+    private void writeConfig(String filename, String content) throws IOException {
+        Path outputDir = Paths.get(outputPath);
+        if(!Files.exists(outputDir)) {
+            Files.createDirectories(outputDir);
+        }
+
+        Path outputFile = outputDir.resolve(filename);
+        Files.writeString(outputFile, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+}
