84c8398fb59cf3c37a145a877ccde019deadf26b
hrishikesh <hrishikeshajith0@gmail.com>
2026-01-03 01:55:12 +0530

    feat: enhance JwtAuthConverter to extract permissions and add logging

--- src/main/java/org/tornotron/echno_backend/common/configuration/JwtAuthConverter.java
+++ src/main/java/org/tornotron/echno_backend/common/configuration/JwtAuthConverter.java
@@ -1,5 +1,6 @@
 package org.tornotron.echno_backend.common.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
+@Slf4j
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
@@ -31,14 +33,30 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
 
     @Override
     public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
-        Collection<GrantedAuthority> authorities = Stream.concat(
-                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                extractResourceRoles(jwt).stream()
-        ).collect(Collectors.toSet());
+        Collection<GrantedAuthority> standardAuthorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+        Collection<? extends GrantedAuthority> resourceRoles = extractResourceRoles(jwt);
+        Collection<? extends GrantedAuthority> permissions = extractPermissions(jwt);
+
+        Collection<GrantedAuthority> authorities = Stream.of(
+                standardAuthorities,
+                resourceRoles,
+                permissions
+        )
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+
+        String principal = getPrincipleClaimName(jwt);
+
+        log.debug("JWT converted for principal: {}", principal);
+        log.debug("Standard authorities: {}", standardAuthorities);
+        log.debug("Resource roles: {}", resourceRoles);
+        log.debug("Extracted permissions: {}", permissions);
+        log.debug("Total authorities: {}", authorities);
+
         return new JwtAuthenticationToken(
                 jwt,
                 authorities,
-                getPrincipleClaimName(jwt)
+                principal
         );
     }
 
@@ -51,23 +69,57 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
     }
 
     private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        Map<String,Object> resourceAccess;
-        Map<String,Object> resource;
-        Collection<String> resourceRoles;
-        if(jwt.getClaim("resource_access") == null) {
-            return Set.of();
-        }
-        resourceAccess = jwt.getClaim("resource_access");
-        if(resourceAccess.get(resourceId) == null) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess == null || resourceAccess.get(resourceId) == null) {
             return Set.of();
         }
 
-        resource = (Map<String, Object>) resourceAccess.get(resourceId);
-        resourceRoles = (Collection<String>) resource.get("roles");
+        Map<String, Object> resource =
+                (Map<String, Object>) resourceAccess.get(resourceId);
 
-        return resourceRoles
-                .stream()
+        Collection<String> roles =
+                (Collection<String>) resource.get("roles");
+
+        return roles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+
+
+
+    private Collection<? extends GrantedAuthority> extractPermissions(Jwt jwt) {
+        Map<String, Object> authorization = jwt.getClaim("authorization");
+        if(authorization == null) {
+            log.debug("No 'authorization' claim found in JWT - RPT token may not have been obtained");
+            return Set.of();
+        }
+
+        Collection<Map<String, Object>> permissions =
+                (Collection<Map<String, Object>>) authorization.get("permissions");
+
+        if (permissions == null) {
+            log.debug("No 'permissions' found in authorization claim");
+            return Set.of();
+        }
+
+        log.debug("Found {} permission(s) in authorization claim", permissions.size());
+
+        return permissions.stream()
+                .flatMap(permission -> {
+                    String resource = (String) permission.get("rsname");
+                    Collection<String> scopes = (Collection<String>) permission.get("scopes");
+
+                    if(scopes == null) {
+                        log.debug("No scopes found for resource: {}", resource);
+                        return Stream.empty();
+                    }
+
+                    log.debug("Resource '{}' has scopes: {}", resource, scopes);
+
+                    return scopes.stream()
+                            .map(scope ->
+                                    new SimpleGrantedAuthority(resource + ":" + scope));
+                })
                 .collect(Collectors.toSet());
     }
 }
