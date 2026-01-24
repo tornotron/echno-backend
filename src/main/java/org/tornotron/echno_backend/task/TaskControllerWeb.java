782a4c4bc91f49f9fdc81aebc8ec92f26da68140
hrishikesh <hrishikeshajith0@gmail.com>
2026-01-24 20:50:40 +0530

    feat: enhance task creation to support file uploads and JSON data

--- src/main/java/org/tornotron/echno_backend/task/TaskControllerWeb.java
+++ src/main/java/org/tornotron/echno_backend/task/TaskControllerWeb.java
@@ -1,15 +1,20 @@
 package org.tornotron.echno_backend.task;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.tornotron.echno_backend.common.response.ApiResponse;
+import org.tornotron.echno_backend.common.service.AttachmentService;
 import org.tornotron.echno_backend.task.dto.TaskCreationDto;
 import org.tornotron.echno_backend.task.dto.TaskDto;
 import org.tornotron.echno_backend.task.dto.TaskPatchDto;
@@ -24,6 +29,7 @@ import java.util.Map;
 public class TaskControllerWeb {
 
     private final TaskService service;
+    private final ObjectMapper objectMapper;
     /** Logger for this class. */
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
@@ -32,20 +38,23 @@ public class TaskControllerWeb {
      *
      * @param service The service for handling task-related business logic.
      */
-    public TaskControllerWeb(TaskService service) {
+    public TaskControllerWeb(TaskService service,ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
         this.service = service;
     }
 
     /**
      * Creates a new task.
      *
-     * @param taskCreationDto DTO containing the details for the new task.
+     *  taskCreationDto DTO containing the details for the new task.
      * @return A {@link ResponseEntity} with a success message and HTTP status 201 (Created).
      */
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PreAuthorize("hasAuthority('task:create') or hasAuthority('task:admin')")
-    public ResponseEntity<TaskSimpleDto> createTask(@Valid @RequestBody TaskCreationDto taskCreationDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.addTask(taskCreationDto));
+    public ResponseEntity<TaskSimpleDto> createTask(@RequestPart @Valid String data,
+                                                    @RequestParam(value = "attachments",required = false)List<MultipartFile> attachments) throws JsonProcessingException {
+        TaskCreationDto dto = objectMapper.readValue(data, TaskCreationDto.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addTask(dto,attachments));
     }
 
     /**
