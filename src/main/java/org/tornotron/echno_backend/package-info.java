25b8d0617511694cf79150db118070bf09daa673
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-10 01:15:52 +0530

    feat: fixed filterDef annotation call from single point

---
+++ src/main/java/org/tornotron/echno_backend/package-info.java
@@ -0,0 +1,5 @@
+@FilterDef(name = "orgFilter", parameters = @ParamDef(name = "organizationId", type = Long.class))
+package org.tornotron.echno_backend;
+
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
