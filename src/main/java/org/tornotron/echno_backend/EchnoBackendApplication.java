49c7ba0f0c69b03af5835edf95daf667962d01f6
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-10 01:27:46 +0530

    feat: proper logs for tenant and hibernate filters

--- src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
+++ src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
@@ -2,11 +2,13 @@ package org.tornotron.echno_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.Ordered;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableCaching
+@EnableTransactionManagement(order = Ordered.HIGHEST_PRECEDENCE)
 public class EchnoBackendApplication {
 
     public static void main(String[] args) {
