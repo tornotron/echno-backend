3189bf534d7e116b23187f8f40bc1d13a75feb0c
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-14 00:51:04 +0530

    refactor: updated user roles

--- src/main/java/org/tornotron/echno_backend/user/enums/UserRole.java
+++ src/main/java/org/tornotron/echno_backend/user/enums/UserRole.java
@@ -1,56 +1,11 @@
 package org.tornotron.echno_backend.user.enums;
 
 public enum UserRole {
-    laborer,
-    electrician,
-    plumber,
-    carpenter,
-    mason,
-    welder,
-    painter,
-    scaffolder,
-    equipmentOperator,
-    craneOperator,
-    driver,
-    helper,
-    siteCleaner,
-    securityGuard,
-
-    // Office & Administrative Roles
-    hrManager,
-    accountant,
-    adminStaff,
-    receptionist,
-    documentController,
-    itSupport,
-    officeAssistant,
-
-    // Engineering, Design & Management Roles
-    civilEngineer,
-    siteEngineer,
-    structuralEngineer,
-    architect,
-    quantitySurveyor,
-    safetyOfficer,
-    planningEngineer,
-    technicalCoordinator,
-    projectManager,
-    siteManager,
-    supervisor,
-    foreman,
-
-    // Contractual & External Roles
-    contractor,
-    subcontractor,
-    materialSupplier,
-    procurementOfficer,
-    vendor,
-    consultant,
-    ownerRepresentative,
-    client,
-
-    // Education & Intern Roles
-    student,
-    intern,
-    trainee,
+    OWNER,
+    CO_FOUNDER,
+    HR_MANAGER,
+    EMPLOYEE,
+    STUDENT,
+    MANAGEMENT,
+    ADMINISTRATOR
 }
