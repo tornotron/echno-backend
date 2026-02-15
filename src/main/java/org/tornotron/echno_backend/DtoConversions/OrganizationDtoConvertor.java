15a2ac1ee8cf1b7337073bf0cb2165b5eb37bf10
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-16 01:16:38 +0530

    refactor: removed teamMember mappers

--- src/main/java/org/tornotron/echno_backend/DtoConversions/OrganizationDtoConvertor.java
+++ src/main/java/org/tornotron/echno_backend/DtoConversions/OrganizationDtoConvertor.java
@@ -1,8 +1,6 @@
 package org.tornotron.echno_backend.DtoConversions;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.tornotron.echno_backend.common.entity.Attachment;
 import org.tornotron.echno_backend.common.entity.AttachmentDto;
 import org.tornotron.echno_backend.common.service.FileStorageService;
@@ -11,73 +9,12 @@ import org.tornotron.echno_backend.employee.dto.EmployeeDto;
 import org.tornotron.echno_backend.organization.Organization;
 import org.tornotron.echno_backend.organization.dto.OrganizationDto;
 import org.tornotron.echno_backend.organization.dto.OrganizationSimpleDto;
-import org.tornotron.echno_backend.project.Project;
-import org.tornotron.echno_backend.project.dto.ProjectDto;
-import org.tornotron.echno_backend.task.Task;
-import org.tornotron.echno_backend.task.dto.TaskDto;
-import org.tornotron.echno_backend.teamMember.TeamMember;
-import org.tornotron.echno_backend.teamMember.dto.TeamMemberDto;
 
 import java.time.Duration;
 import java.util.stream.Collectors;
 
 @Component
 public class OrganizationDtoConvertor {
-
-
-    private static TeamMemberDto convertTeamMemberToTeamMemberDTO(TeamMember teamMember) {
-        TeamMemberDto teamMemberDto = new TeamMemberDto();
-        teamMemberDto.setId(teamMember.getId());
-        teamMemberDto.setMemberName(teamMember.getMemberName());
-        teamMemberDto.setMemberEmail(teamMember.getMemberEmail());
-        teamMemberDto.setMemberPhone(teamMember.getMemberPhone());
-        teamMemberDto.setMemberRole(teamMember.getMemberRole());
-        teamMemberDto.setMemberImage(teamMember.getMemberImage());
-        return teamMemberDto;
-    }
-
-    private static TaskDto convertTaskToTaskDto(Task task,FileStorageService fileStorageService) {
-        TaskDto taskDto = new TaskDto();
-        taskDto.setId(task.getId());
-        taskDto.setTitle(task.getTitle());
-        taskDto.setStartDate(task.getStartDate());
-        taskDto.setEndDate(task.getEndDate());
-        taskDto.setCreator(EmployeeDtoConvertor.convertEmployeeToDto(task.getCreator(),fileStorageService));
-        taskDto.setProjectId(task.getProject().getId());
-        taskDto.setProgress(task.getProgress());
-        taskDto.setAssignees(task.getAssignees().stream()
-                .map(employee -> EmployeeDtoConvertor.convertEmployeeToDto(employee,fileStorageService))
-                .collect(Collectors.toSet()));
-        taskDto.setCategory(CategoryDtoConvertor.convertCategoryToDto(task.getCategory()));
-        taskDto.setTags(task.getTags());
-        taskDto.setCreatedAt(task.getCreatedAt());
-        taskDto.setUpdatedAt(task.getUpdatedAt());
-        taskDto.setStatus(task.getStatus());
-        taskDto.setIssues(task.getIssues().stream()
-                .map(IssueDtoConvertor::convertIssueToDto)
-                .collect(Collectors.toList()));
-        return taskDto;
-    }
-
-    private static ProjectDto convertProjectToProjectDto(Project project,FileStorageService fileStorageService) {
-        ProjectDto projectDto = new ProjectDto();
-        projectDto.setId(project.getId());
-        projectDto.setProjectName(project.getProjectName());
-        projectDto.setProjectAddress(project.getProjectAddress());
-        projectDto.setStatus(project.getStatus());
-        projectDto.setProjectLongitude(project.getProjectLongitude());
-        projectDto.setProjectLatitude(project.getProjectLatitude());
-        projectDto.setStartDate(project.getStartDate());
-        projectDto.setEndDate(project.getEndDate());
-        projectDto.setCreatedAt(project.getCreatedAt());
-        projectDto.setTeamMembers(project.getTeamMembers().stream()
-                .map(OrganizationDtoConvertor::convertTeamMemberToTeamMemberDTO)
-                .collect(Collectors.toList()));
-        projectDto.setTasks(project.getTasks().stream()
-                .map(task ->TaskDtoConvertor.convertTaskToDto(task,fileStorageService))
-                .collect(Collectors.toList()));
-        return projectDto;
-    }
 
     private static EmployeeDto convertEmployeeToEmployeeDto(Employee employee) {
         EmployeeDto employeeDto = new EmployeeDto();
