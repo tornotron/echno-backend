1af60571f6779a9fd163af63e1995fd1f97e6508
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-20 23:47:10 +0530

    feat: added attachment conversion and mapping support in ProjectDto

--- src/main/java/org/tornotron/echno_backend/project/dto/ProjectSimpleDto.java
+++ src/main/java/org/tornotron/echno_backend/project/dto/ProjectSimpleDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.tornotron.echno_backend.project.enums.ProjectCreationStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class ProjectSimpleDto {
