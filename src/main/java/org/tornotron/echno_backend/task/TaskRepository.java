d74e95fb8e5e4a0f482eae559ee28fb06953293f
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-24 23:59:37 +0530

    feat: added task retrieval by projectId, project progress updates, and improved attachment handling in Task entity and service

--- src/main/java/org/tornotron/echno_backend/task/TaskRepository.java
+++ src/main/java/org/tornotron/echno_backend/task/TaskRepository.java
@@ -3,7 +3,9 @@ package org.tornotron.echno_backend.task;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.tornotron.echno_backend.IssueComment.IssueComment;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -23,7 +25,11 @@ public interface TaskRepository extends JpaRepository<Task,Long> {
     Optional<Task> findByIdAndOrganization_Id(Long id, Long organizationId);
 
     boolean existsByIdAndOrganization_Id(Long id, Long organizationId);
-            
+
+    List<Task> findByProject_Id(Long projectId);
+
+    List<Task> findAllByProject_IdAndOrganization_Id(Long projectId, Long organizationId);
+
 
 //    List<Task> findTaskByEmployee_IdAndProject_Id(@NotNull(message = "employeeId is required") Long employeeId, @NotNull(message = "projectId is required") Long projectId);
 }
