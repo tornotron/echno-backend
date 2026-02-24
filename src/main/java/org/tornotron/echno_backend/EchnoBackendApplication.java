5e03848c2de0d6cedd23ddddf936a4f78def1507
hrishikesh <hrishikeshajith0@gmail.com>
2026-02-24 23:56:11 +0530

    feat: refactored Keycloak initialization with async support, updated Keycloak issuer URI config, and adjusted log levels to reduce noise

--- src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
+++ src/main/java/org/tornotron/echno_backend/EchnoBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.core.Ordered;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 @EnableTransactionManagement(order = Ordered.HIGHEST_PRECEDENCE)
 public class EchnoBackendApplication {
 
