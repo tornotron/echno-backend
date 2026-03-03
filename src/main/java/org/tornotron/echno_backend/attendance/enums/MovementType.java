abeed96918ead354277faf4ecf53de09c2d00bb3
hrishikesh <hrishikeshajith0@gmail.com>
2026-03-03 21:27:28 +0530

    feat: introduced MovementRecord functionality with entities, DTOs, controllers, mappers, repository, services, and enums in attendance module

---
+++ src/main/java/org/tornotron/echno_backend/attendance/enums/MovementType.java
@@ -0,0 +1,15 @@
+package org.tornotron.echno_backend.attendance.enums;
+
+public enum MovementType {
+    SITE_TRAVEL,
+    CLIENT_MEETING,
+    VENDOR_MEETING,
+    WORK_FROM_HOME,
+    ON_FIELD_WORK,
+    TRAINING,
+    OFFICE_WORK,
+    INSPECTION,
+    MATERIAL_PROCUREMENT,
+    SUPERVISORY_VISIT,
+    OTHER
+}
