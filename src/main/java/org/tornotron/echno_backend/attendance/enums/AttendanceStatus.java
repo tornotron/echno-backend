cded53e6d14c34d54966d2916ae32e5e47644fc2
hrishikesh <hrishikeshajith0@gmail.com>
2026-03-03 21:26:50 +0530

    feat: added AttendanceStatus enum in attendance module to define various attendance states

---
+++ src/main/java/org/tornotron/echno_backend/attendance/enums/AttendanceStatus.java
@@ -0,0 +1,14 @@
+package org.tornotron.echno_backend.attendance.enums;
+
+public enum AttendanceStatus {
+    PRESENT,
+    HALF_DAY,
+    ABSENT,
+    LEAVE,
+    WEEKLY_OFF,
+    HOLIDAY,
+    LATE,
+    EARLY_CHECKOUT,
+    OVERTIME,
+    PENDING_REGULARIZATION
+}
