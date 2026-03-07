bde2899f87f8976db87a26a03ff3d6d5a08a590d
hrishikesh <hrishikeshajith0@gmail.com>
2026-03-07 21:09:58 +0530

    feat: added extended repository layer for vendor management including additional methods and new repositories

---
+++ src/main/java/org/tornotron/echno_backend/vendor/VendorPaymentTermsRepository.java
@@ -0,0 +1,6 @@
+package org.tornotron.echno_backend.vendor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorPaymentTermsRepository extends JpaRepository<VendorPaymentTerms, Long> {
+}
--- src/main/java/org/tornotron/echno_backend/vendor/VendorRepository.java
+++ src/main/java/org/tornotron/echno_backend/vendor/VendorRepository.java
@@ -1,7 +1,10 @@
 package org.tornotron.echno_backend.vendor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.tornotron.echno_backend.vendor.enums.VendorStatus;
+import org.tornotron.echno_backend.vendor.enums.VendorType;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +15,15 @@ public interface VendorRepository extends JpaRepository<Vendor, Long> {
     List<Vendor> findByVendorNameContainingIgnoreCase(String vendorName);
 
     boolean existsByVendorEmail(String vendorEmail);
+
+    Optional<Vendor> findByIdAndOrganization_Id(Long id, Long organizationId);
+    
+    boolean existsByIdAndOrganization_Id(Long id, Long organizationId);
+
+    void deleteByIdAndOrganization_Id(Long id, Long organizationId);
+
+    List<Vendor> findAllByStatus(VendorStatus status);
+
+    List<Vendor> findAllByType(VendorType type);
+
 }
