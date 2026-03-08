11e2aa8d818517e0c5d580a4add6a915f9942fff
hrishikesh <hrishikeshajith0@gmail.com>
2026-03-08 21:19:41 +0530

    feat: extended vendor management with child entity APIs, organization scoping, and CRUD support

--- src/main/java/org/tornotron/echno_backend/vendor/VendorBankAccountRepository.java
+++ src/main/java/org/tornotron/echno_backend/vendor/VendorBankAccountRepository.java
@@ -2,5 +2,9 @@ package org.tornotron.echno_backend.vendor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VendorBankAccountRepository extends JpaRepository<VendorBankAccountRepository, Long> {
+import java.util.List;
+
+public interface VendorBankAccountRepository extends JpaRepository<VendorBankAccount, Long> {
+
+    List<VendorBankAccount> findByVendor_Id(Long vendorId);
 }
--- src/main/java/org/tornotron/echno_backend/vendor/VendorPaymentTermsRepository.java
+++ src/main/java/org/tornotron/echno_backend/vendor/VendorPaymentTermsRepository.java
@@ -2,5 +2,9 @@ package org.tornotron.echno_backend.vendor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VendorPaymentTermsRepository extends JpaRepository<VendorPaymentTerms, Long> {
+
+    Optional<VendorPaymentTerms> findByVendor_Id(Long vendorId);
 }
