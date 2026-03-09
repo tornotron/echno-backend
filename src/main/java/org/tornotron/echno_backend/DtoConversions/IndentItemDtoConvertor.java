78c198e4c218968cd0b3b2eebbf3ba8b6c933570
hrishikesh <hrishikeshajith0@gmail.com>
2026-03-09 15:22:38 +0530

    feat: migrated SiteTransfer `sendingPerson` field to Employee entity with lazy loading and updated DTO conversions, adjusted related services and validations

--- src/main/java/org/tornotron/echno_backend/DtoConversions/IndentItemDtoConvertor.java
+++ src/main/java/org/tornotron/echno_backend/DtoConversions/IndentItemDtoConvertor.java
@@ -1,20 +1,21 @@
 package org.tornotron.echno_backend.DtoConversions;
 
 import org.springframework.stereotype.Component;
+import org.tornotron.echno_backend.common.service.FileStorageService;
 import org.tornotron.echno_backend.indentItem.IndentItem;
 import org.tornotron.echno_backend.indentItem.dto.IndentItemDto;
 
 @Component
 public class IndentItemDtoConvertor {
 
-    public static IndentItemDto convertIndentItemToDto(IndentItem item) {
+    public static IndentItemDto convertIndentItemToDto(IndentItem item, FileStorageService fileStorageService) {
         if (item == null) {
             return null;
         }
 
         IndentItemDto dto = new IndentItemDto();
         dto.setId(item.getId());
-        dto.setMaterial(MaterialDtoConvertor.convertToDto(item.getMaterial()));
+        dto.setMaterial(MaterialDtoConvertor.convertToDto(item.getMaterial(), fileStorageService));
         dto.setAdditionalSpecifications(item.getAdditionalSpecifications());
         dto.setRequestedQuantity(item.getRequestedQuantity());
         dto.setOrderedQuantity(item.getOrderedQuantity());
--- src/main/java/org/tornotron/echno_backend/siteTransfer/dto/SiteTransferDto.java
+++ src/main/java/org/tornotron/echno_backend/siteTransfer/dto/SiteTransferDto.java
@@ -1,6 +1,7 @@
 package org.tornotron.echno_backend.siteTransfer.dto;
 
 import lombok.Data;
+import org.tornotron.echno_backend.employee.dto.EmployeeDto;
 import org.tornotron.echno_backend.siteTransfer.enums.SiteTransferStatus;
 import org.tornotron.echno_backend.user.dto.UserDto;
 
@@ -13,7 +14,7 @@ public class SiteTransferDto {
     private Long id;
     private String transferNumber;
     private LocalDateTime issueDate;
-    private UserDto sendingPerson;
+    private EmployeeDto sendingPerson;
     private String receivingSite;
     private SiteTransferStatus status;
     private List<SiteTransferItemDto> items;
