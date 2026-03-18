14c7d5ec7d433c4c0cbc4f5c50a31e2890fec95d
hrishikesh <hrishikeshajith0@gmail.com>
2026-03-19 02:10:48 +0530

    feat: updated VendorType enum with additional categories

--- src/main/java/org/tornotron/echno_backend/vendor/enums/VendorType.java
+++ src/main/java/org/tornotron/echno_backend/vendor/enums/VendorType.java
@@ -1,5 +1,9 @@
 package org.tornotron.echno_backend.vendor.enums;
 
 public enum VendorType {
-    SUPPLIER, CONTRACTOR, SERVICE_PROVIDER, CONSULTANT, TRANSPORTER
+    MATERIALS,
+    EQUIPMENTS,
+    SERVICES,
+    TRANSPORT,
+    OTHERS
 }
