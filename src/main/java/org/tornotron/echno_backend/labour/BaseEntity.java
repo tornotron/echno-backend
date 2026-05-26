f271a320a69d54e998bf2557b4d801e7ee3dc3fe
Hrishikesh Ajith <hrishikeshajith0@gmail.com>
2026-05-26 12:27:45 +0530

    feat: abstract class base entity for labour module

---
+++ src/main/java/org/tornotron/echno_backend/labour/BaseEntity.java
@@ -0,0 +1,25 @@
+package org.tornotron.echno_backend.labour;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
